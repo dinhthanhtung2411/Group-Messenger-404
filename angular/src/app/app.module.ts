3b48292ecc24e6980e437be96386d42a0a3cc390
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-10 16:01:46 +0700

    complete base search

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -8,13 +8,15 @@ import {FormsModule} from '@angular/forms';
 import { ListMemberComponent } from './list-member/list-member.component';
 import { GroupMessengerComponent } from './group-messenger/group-messenger.component';
 import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InviteFormComponent,
     GroupMessengerComponent,
-    RegisterComponent  ],
+    RegisterComponent,
+    SearchComponent  ],
     imports: [
         BrowserModule,
         AppRoutingModule,
