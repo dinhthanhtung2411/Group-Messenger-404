3f5d1e77945f2f7cc895b0d0ab26227244bfd38f
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-10 10:30:32 +0700

    complete base login

--- angular/src/app/invite-form/invite-form.component.ts
+++ angular/src/app/invite-form/invite-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../service/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-invite-form',
@@ -8,13 +9,17 @@ import {AuthService} from '../service/auth.service';
 })
 export class InviteFormComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private route: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(valueForm) {
     console.log(valueForm.name);
-    this.authService.login(valueForm);
+    if (this.authService.login(valueForm)) {
+      this.route.navigate(['/']);
+    }
   }
 }
--- angular/src/app/service/auth.service.ts
+++ angular/src/app/service/auth.service.ts
@@ -8,9 +8,9 @@ export class AuthService {
   constructor() { }
 
   login(account) {
-    console.log(account);
     if (account.name === 'hai' && account.psw === '123123') {
-
+      return true;
     }
+    return false;
   }
 }
