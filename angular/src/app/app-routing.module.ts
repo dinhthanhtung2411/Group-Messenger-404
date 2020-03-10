7df77d7a40eb9dbab5d24ed3f7911acbb4b6d94b
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-10 09:38:30 +0700

    init form-login

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InviteFormComponent} from './invite-form/invite-form.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {
+      path: 'login',
+      component: InviteFormComponent
+    },
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AComponent } from './a/a.component';
 import { InviteFormComponent } from './invite-form/invite-form.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AComponent,
     InviteFormComponent
   ],
     imports: [
